2547116f0cd62e358bc0331c034050b6b471bf4c
mbright90 <michael.bright90@icloud.com>
2024-02-17 17:10:36 +0000

    Import App and implement in entry

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import App from "./app";
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(document.getElementById("root")!);
 
 root.render(
     <React.StrictMode>
-        <div>Hello world</div>
+        <App />
     </React.StrictMode>
 );
