26b0ce5f111eaaf935a8eaabfac4a63a956c633e
mbright90 <michael.bright90@icloud.com>
2024-02-17 17:18:11 +0000

    Set out initial components and style links

---
+++ client/src/app/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '@style/style.scss';
+
+import Footer from "@components/footer";
+
+const App: React.FC = () => {
+    return (
+        <>
+            <div>hello world!</div>
+            <Footer />
+        </>
+    );
+};
+
+export default App;
---
+++ client/src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="footer">
+            <a href="https://github.com/MBright90/vision_vault_inventory">MBright90</a>
+        </footer>
+    );
+};
+
+export default Footer;
