12fd97f99b27b225dd2bd613e4d2fb7c7b58e24d
mbright90 <michael.bright90@icloud.com>
2024-02-17 16:36:13 +0000

    Setup module declaration and index.tsx

---
+++ client/src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+    <React.StrictMode>
+        <div>Hello world</div>
+    </React.StrictMode>
+);
