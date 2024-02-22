9593ab9122b51ce3a577871dca9c12de3adb0fbd
mbright90 <michael.bright90@icloud.com>
2024-02-22 10:27:33 +0000

    Add functions to update product stock count

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable camelcase */
 const Product = require('../models/product');
 
 // product_get, product_post, product_put, product_delete
 
-function product_details_get(req, res) {
+function product_get(req, res) {
   const { id } = req.params;
 
   Product.findById(id)
@@ -15,7 +16,7 @@ function product_details_get(req, res) {
     });
 }
 
-function product_put(req, res) {
+function product_post(req, res) {
   const {
     name, description, price, number_in_stock, image,
   } = req.body;
@@ -31,7 +32,34 @@ function product_put(req, res) {
     });
 }
 
+// function product_put(req, res) {
+//   const { id } = req.params;
+
+//   Product.EditById;
+// }
+
+function product_update_stock(req, res) {
+  const { id } = req.params;
+  const { increment } = req.body;
+
+  Product.findOneAndUpdate({ _id: id }, { $inc: { number_in_stock: increment } })
+    .then((result) => {
+      res.send(result);
+    }).catch((err) => res.status(500).send(err));
+}
+
+function product_decrement_stock(req, res) {
+  const { id } = req.params;
+
+  Product.findOneAndUpdate({ _id: id }, { $inc: { number_in_stock: -1 } })
+    .then((result) => {
+      res.send(result);
+    }).catch((err) => res.status(500).send(err));
+}
+
 module.exports = {
-  product_details_get,
-  product_put,
+  product_get,
+  product_post,
+  product_update_stock,
+  product_decrement_stock,
 };
