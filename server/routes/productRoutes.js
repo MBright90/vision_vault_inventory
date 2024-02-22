7d4d44eca79b98c00d4830f68e6972e375e9921b
mbright90 <michael.bright90@icloud.com>
2024-02-22 10:27:19 +0000

    Add additional product routes

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -6,6 +6,14 @@ const productRouter = express.Router();
 // productRouter.get('/', (req, res) => {
 // });
 
-productRouter.post('/', productController.product_put);
+productRouter.get('/search/', productController.product_search);
+
+productRouter.get('/:id', productController.product_get);
+
+productRouter.post('/', productController.product_post);
+
+productRouter.put('/update_stock/:id', productController.product_update_stock);
+
+productRouter.put('/decrement/:id', productController.product_decrement_stock);
 
 module.exports = productRouter;
