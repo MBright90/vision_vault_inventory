b4e1f5bc6764f9b7e45b9fe4ce78561f061fd089
mbright90 <michael.bright90@icloud.com>
2024-02-24 10:29:45 +0000

    Add genre parsing logic

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable camelcase */
 const Product = require('../models/product');
+const genreController = require('./genreController');
 
 // product_get, product_post, product_put, product_delete
 
@@ -18,11 +19,29 @@ function product_get(req, res) {
 
 function product_post(req, res) {
   const {
-    name, description, price, number_in_stock, image,
+    name, description, price, number_in_stock, image, genres, type,
   } = req.body;
 
+  // parse genres
+  const genreArr = genres.split(' ');
+  const genreDocs = [];
+
+  genreArr.forEach((genre) => {
+    const genreDoc = genreController.genre_get_id(req, res, genre);
+    if (!genreDoc._id) return;
+    genreDocs[genreDocs.length] = genreDoc;
+  });
+
   const newProduct = new Product({
-    name, description, price, number_in_stock, image,
+    name,
+    description,
+    price,
+    number_in_stock,
+    image,
+    genreDocs,
+    type,
+    stock_last_updated: new Date(),
+    last_updated: new Date(),
   });
 
   newProduct.save()
@@ -32,17 +51,14 @@ function product_post(req, res) {
     });
 }
 
-// function product_put(req, res) {
-//   const { id } = req.params;
-
-//   Product.EditById;
-// }
-
 function product_update_stock(req, res) {
   const { id } = req.params;
   const { increment } = req.body;
 
-  Product.findOneAndUpdate({ _id: id }, { $inc: { number_in_stock: increment } })
+  Product.findOneAndUpdate(
+    { _id: id },
+    { $inc: { number_in_stock: increment }, $set: { stock_last_updated: new Date() } },
+  )
     .then((result) => {
       res.send(result);
     }).catch((err) => res.status(500).send(err));
@@ -51,7 +67,10 @@ function product_update_stock(req, res) {
 function product_decrement_stock(req, res) {
   const { id } = req.params;
 
-  Product.findOneAndUpdate({ _id: id }, { $inc: { number_in_stock: -1 } })
+  Product.findOneAndUpdate(
+    { _id: id },
+    { $inc: { number_in_stock: -1 }, $set: { stock_last_updated: new Date() } },
+  )
     .then((result) => {
       res.send(result);
     }).catch((err) => res.status(500).send(err));
