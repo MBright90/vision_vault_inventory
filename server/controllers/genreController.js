7e2dbfb7f28b58aa7a28cfe3524225d56ca7b754
mbright90 <michael.bright90@icloud.com>
2024-02-24 10:29:57 +0000

    Find and upsert genres when creating product

--- server/controllers/genreController.js
+++ server/controllers/genreController.js
@@ -3,12 +3,25 @@ const Genre = require('../models/genre');
 
 // genre_get_all
 
+function genre_get_id(req, res, genre, genreArr) {
+  // find genre and create if not found
+  Genre.findOneAndUpdate({ name: genre }, { upsert: true })
+    .then((result) => result)
+    .catch((err) => res.status(500).send(err));
+
+  // Potential efficient method
+  Genre.updateMany({ name: { $in: genreArr } }, { upsert: true })
+    .then((result) => result)
+    .catch((err) => res.status(500).send(err));
+}
+
 function genre_get_all(req, res) {
   Genre.find()
     .sort({ name: 1 })
     .then((result) => {
       res.send(result); // Add actual information parsing here
-    });
+    })
+    .catch((err) => res.status(500).send(err));
 }
 
 function genre_get_products(req, res) {
@@ -20,6 +33,7 @@ function genre_get_products(req, res) {
 }
 
 module.exports = {
+  genre_get_id,
   genre_get_all,
   genre_get_products,
 };
