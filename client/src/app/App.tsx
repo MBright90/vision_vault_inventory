72022ddecda402a3f1f224154b78a1b46e3137dd
mbright90 <michael.bright90@icloud.com>
2024-02-25 10:19:03 +0000

    Import and display display component

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import '@style/style.scss';
 
 import Footer from "@components/footer";
+import Display from "@components/display";
 import Navbar from "@components/navbar";
 
 const App: React.FC = () => {
     return (
         <>
             <Navbar />
-            <div>hello world!</div>
+            <Display />
             <Footer />
         </>
     );
