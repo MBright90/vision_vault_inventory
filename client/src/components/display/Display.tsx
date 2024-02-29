457af1b64f3aee218501908e09ce91d44b74f14c
mbright90 <michael.bright90@icloud.com>
2024-02-29 17:05:36 +0000

    Resolve misused Promise

--- client/src/components/display/Display.tsx
+++ client/src/components/display/Display.tsx
@@ -3,10 +3,19 @@ import React from "react";
 import style from './Display.module.scss';
 
 const Display: React.FC = () => {
+    const checkConnection = async (): Promise<void> => {
+        const result = await fetch('http://localhost:3000/').catch((err) => { console.log(err); });
+        if (result === null) return;
+        
+        console.log(result);
+    };
+
     return (
         <main className={style.display}>
             <div className={style.displayControls}></div>
-            <div className={style.displayScreen}></div>
+            <div className={style.displayScreen}>
+                <button onClick={() => { void checkConnection(); }}>Call api</button>
+            </div>
         </main>
     );
 };
