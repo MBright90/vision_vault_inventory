fca4985f43a306841cc1b53201b4c9bbbf6c95f8
mbright90 <michael.bright90@icloud.com>
2024-03-06 16:58:34 +0000

    Add unique attribute

--- server/models/type.js
+++ server/models/type.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const TypeSchema = new mongoose.Schema({
   name: {
     type: String,
-    enum: ['hardware', 'audio/disc', 'video/disc', 'game/disc'],
+    enum: ['hardware', 'video/disc', 'game/disc', 'book'],
     required: true,
+    unique: true,
   },
   products: [{ type: mongoose.Types.ObjectId, ref: 'product' }],
 });
