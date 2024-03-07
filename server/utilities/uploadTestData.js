d867bbc983764c1dde185773792a27776f2c0a09
mbright90 <michael.bright90@icloud.com>
2024-03-07 16:38:12 +0000

    Add typeId retrieval

--- server/utilities/uploadTestData.js
+++ server/utilities/uploadTestData.js
@@ -2,10 +2,11 @@
 const mongoose = require('mongoose');
 
 // models
-const Product = require('../models/product');
+// const Product = require('../models/product');
 
 // controllers
 const genreController = require('../controllers/genreController');
+const typeController = require('../controllers/typeController');
 
 // data
 const testData = require('./testData/productData.json');
@@ -22,35 +23,43 @@ async function uploadTestProducts(dataArr) {
   const mappedData = await Promise.all(dataArr.map(async (product) => {
     const genreArr = product.genres.split(',').map((item) => item.trim());
 
-    let genreIDs = [];
+    let genreIds = [];
 
     if (genreArr.length > 0) {
-      genreIDs = await Promise.all(genreArr.map(async (genre) => {
-        const ID = await genreController.get_id(null, null, genre);
+      genreIds = await Promise.all(genreArr.map(async (genre) => {
+        const ID = await genreController.get_id(genre);
+        console.log(ID);
         return ID;
       }));
     }
 
+    const typeId = await typeController.get_id(product.type);
+
     return {
-      ...product, genres: genreIDs, last_updated: new Date(), stock_last_updated: new Date(),
+      ...product,
+      genres: genreIds,
+      type: typeId,
+      last_updated: new Date(),
+      stock_last_updated: new Date(),
     };
   }));
 
   console.log(mappedData);
 
-  mappedData.forEach((item) => {
-    try {
-      const productDoc = new Product(item);
-      const productID = productDoc.save();
+  //   mappedData.forEach((item) => {
+  //     try {
+  //       const productDoc = new Product(item);
+  //       const productId = productDoc.save();
 
-      productDoc.genres.forEach((genre) => {
-        genreController.add_product(null, null, genre, productID);
-      });
-    } catch (err) {
-      console.log(err);
-    }
-    // Do same for types
-  });
+  //       productDoc.genres.forEach((genre) => {
+  //         genreController.add_product(genre, productId);
+  //       });
+
+//       typeController.add_product(productDoc.type, productId);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   });
 }
 
 main()
