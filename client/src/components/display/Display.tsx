2bf88807e8a691db944d565f6a41dda1a408dc7e
mbright90 <michael.bright90@icloud.com>
2024-03-11 19:49:09 +0000

    Alter fetch path

--- client/src/components/display/Display.tsx
+++ client/src/components/display/Display.tsx
@@ -4,7 +4,7 @@ import style from './Display.module.scss';
 
 const Display: React.FC = () => {
     const checkConnection = async (): Promise<void> => {
-        const result = await fetch('http://localhost:3000/').catch((err) => { console.log(err); });
+        const result = await fetch('http://localhost:3000/genres/').catch((err) => { console.log(err); });
         if (result === null) return;
         
         console.log(result);
