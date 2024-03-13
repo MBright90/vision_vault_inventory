892452151496a303d43deb9d63968bc6919f4536
mbright90 <michael.bright90@icloud.com>
2024-03-13 17:14:18 +0000

    Begin structuring of data flow

--- client/src/components/display/Display.tsx
+++ client/src/components/display/Display.tsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import style from './Display.module.scss';
 import type Genre from "src/custom_types/genre";
 
+const allGenre: Genre = {
+    __id: '',
+    __v: 0,
+    name: 'All',
+    products: ['']
+};
+
 const Display: React.FC = () => {
+    const [genreSelection, setGenreSelection] = useState<Genre[]>([allGenre]);
+    const [genreNodes, setGenreNodes] = useState<React.ReactNode[]>([]);
+
+    // Fetch genres
+    useEffect(() => {
+        (async () => {
+            let data: Genre[] = [];
+            try {
+                const response = await fetch('http://localhost:3000/genres/');
+                if (!response.ok) throw new Error('Failed to retrieve genres');
+                data = await response.json();
+            } catch(err) { console.log(err); }
+            setGenreSelection((prev) => [...prev, ...data]);
+        })();
+    }, []);
+
+    // Change genreSelection to nodes
+    useEffect(() => {
+
+    }, []);
+
     const retrieveGenres = async (): Promise<Genre[]> => {
         let data: Genre[] = [];
         try {
@@ -12,7 +40,6 @@ const Display: React.FC = () => {
                 throw new Error('Failed to retrieve genres');
             }
             data = await result.json();
-            console.log(data);
         } catch(err) {
             console.log(err);
         }
@@ -23,6 +50,7 @@ const Display: React.FC = () => {
         <main className={style.display}>
             <div className={style.displayControls}>
                 <button onClick={() => { void retrieveGenres(); }}>Call api</button>
+                {genreNodes}
             </div>
             <div className={style.displayScreen}>
             </div>
