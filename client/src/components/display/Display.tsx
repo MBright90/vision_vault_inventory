bcacee47677288241b820de53161cfb95382392c
mbright90 <michael.bright90@icloud.com>
2024-03-13 16:58:06 +0000

    Modify typing of API result

--- client/src/components/display/Display.tsx
+++ client/src/components/display/Display.tsx
@@ -1,20 +1,30 @@
 import React from "react";
 
 import style from './Display.module.scss';
+import type Genre from "src/custom_types/genre";
 
 const Display: React.FC = () => {
-    const checkConnection = async (): Promise<void> => {
-        const result = await fetch('http://localhost:3000/genres/').catch((err) => { console.log(err); });
-        if (result === null) return;
-        
-        console.log(result);
+    const retrieveGenres = async (): Promise<Genre[]> => {
+        let data: Genre[] = [];
+        try {
+            const result = await fetch('http://localhost:3000/genres/');
+            if (!result.ok) {
+                throw new Error('Failed to retrieve genres');
+            }
+            data = await result.json();
+            console.log(data);
+        } catch(err) {
+            console.log(err);
+        }
+        return data;
     };
 
     return (
         <main className={style.display}>
-            <div className={style.displayControls}></div>
+            <div className={style.displayControls}>
+                <button onClick={() => { void retrieveGenres(); }}>Call api</button>
+            </div>
             <div className={style.displayScreen}>
-                <button onClick={() => { void checkConnection(); }}>Call api</button>
             </div>
         </main>
     );
