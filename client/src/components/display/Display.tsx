3cf48c40ccd215a5a867f341f5f70f28ed086982
mbright90 <michael.bright90@icloud.com>
2024-03-14 10:34:37 +0000

    Set useEffect for genre retrieval

--- client/src/components/display/Display.tsx
+++ client/src/components/display/Display.tsx
@@ -12,45 +12,34 @@ const allGenre: Genre = {
 
 const Display: React.FC = () => {
     const [genreSelection, setGenreSelection] = useState<Genre[]>([allGenre]);
-    const [genreNodes, setGenreNodes] = useState<React.ReactNode[]>([]);
+    // const [genreNodes, setGenreNodes] = useState<React.ReactNode[]>([]);
 
     // Fetch genres
-    useEffect(() => {
-        (async () => {
-            let data: Genre[] = [];
+    useEffect( (): void => {
+        const retrieveGenres = async (): Promise<void> => {
             try {
                 const response = await fetch('http://localhost:3000/genres/');
-                if (!response.ok) throw new Error('Failed to retrieve genres');
-                data = await response.json();
-            } catch(err) { console.log(err); }
-            setGenreSelection((prev) => [...prev, ...data]);
-        })();
+            if (!response.ok) throw new Error('Failed to retrieve genres');
+                const data = await response.json();
+                setGenreSelection([allGenre, ...data]);
+            } catch(err) {
+                console.log(err);
+            }
+        };
+
+        void retrieveGenres();
     }, []);
 
     // Change genreSelection to nodes
-    useEffect(() => {
-
-    }, []);
-
-    const retrieveGenres = async (): Promise<Genre[]> => {
-        let data: Genre[] = [];
-        try {
-            const result = await fetch('http://localhost:3000/genres/');
-            if (!result.ok) {
-                throw new Error('Failed to retrieve genres');
-            }
-            data = await result.json();
-        } catch(err) {
-            console.log(err);
-        }
-        return data;
-    };
+    useEffect((): void => {
+        console.log(genreSelection);
+    }, [genreSelection]);
 
     return (
         <main className={style.display}>
             <div className={style.displayControls}>
-                <button onClick={() => { void retrieveGenres(); }}>Call api</button>
-                {genreNodes}
+                {/* <button onClick={() => { void retrieveGenres(); }}>Call api</button> */}
+                {/* {genreNodes} */}
             </div>
             <div className={style.displayScreen}>
             </div>
