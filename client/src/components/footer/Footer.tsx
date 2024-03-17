e19d4c572855a6331f1d56ee41f6db782fc02e57
mbright90 <michael.bright90@icloud.com>
2024-03-17 11:57:15 +0000

    Add github brand icon

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 import style from './Footer.module.scss';
 
 const Footer: React.FC = () => {
     return (
         <footer className={style.footer}>
-            <a href="https://github.com/MBright90/vision_vault_inventory">MBright90</a>
+            <a href="https://github.com/MBright90/vision_vault_inventory">MBright90 <FontAwesomeIcon icon={faGithub}/></a>
         </footer>
     );
 };
