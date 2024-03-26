2ee6645cf3a6bd7168f246051e626365de6f1443
mbright90 <michael.bright90@icloud.com>
2024-03-26 14:11:43 +0000

    Layout skeleton product information

--- client/src/components/product/Product.tsx
+++ client/src/components/product/Product.tsx
@@ -7,6 +7,11 @@ interface ProductProps {
     product: product_type
 }
 
+interface product_genre {
+    name: string,
+    _id: string
+}
+
 const Product: React.FC<ProductProps> = ({ product }) => {
     const [isEditing, setIsEditing] = useState<boolean>(false);
 
@@ -14,9 +19,26 @@ const Product: React.FC<ProductProps> = ({ product }) => {
         setIsEditing(false);
     }, [isEditing]);
 
+    const productGenres: React.ReactNode[] = product.genres.map((genre: product_genre) => {
+        return <span key={genre._id}>{genre.name}</span>;
+    });
+
+    // updateStockFunc
+    // startEditing func
+
      return (
         <div className={style.productContainer}>
-            { isEditing ? <p>editing</p>: <p>{product.name}</p>}
+            <p className={style.productName}>{product.name}</p>
+            <p className={style.productDetails}>{product.type.name} / {productGenres}</p>
+            <p className={style.productDescription}>{product.description}</p>
+            <div className={style.inStockContainer}>
+                <p>{product.number_in_stock} <span>Last Updated: {product.stock_last_updated.toDateString()}</span></p>
+                <button>Update stock</button>
+            </div>
+            <div className={style.editContainer}>
+                <button>Edit</button>
+                <p>Last Edited: {}</p>
+            </div>
         </div>
      );
 };
