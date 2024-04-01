2c1fc1cd5e378a5f215ca29d81d38597e6d50916
mbright90 <michael.bright90@icloud.com>
2024-04-01 17:31:36 +0100

    Add modal close ability

--- client/src/components/modal/Modal.tsx
+++ client/src/components/modal/Modal.tsx
@@ -4,13 +4,15 @@ import style from './Modal.module.scss';
 
 interface ModalProps {
     children: React.ReactNode
+    closeModal: () => void
 }
 
-const Modal: React.FC<ModalProps> = ({ children }) => {
+const Modal: React.FC<ModalProps> = ({ children, closeModal }) => {
     return (
         <div className={style.modalBackground}>
             <div className={style.modalContainer}>
                 {children}
+                <button onClick={closeModal} >X</button>
             </div>
         </div>
     );
