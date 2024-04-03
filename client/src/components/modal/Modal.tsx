f5858df7a9ad33cc4deaeb56197cff92dede30fb
mbright90 <michael.bright90@icloud.com>
2024-04-03 10:56:03 +0100

    Implement confirm props

--- client/src/components/modal/Modal.tsx
+++ client/src/components/modal/Modal.tsx
@@ -7,14 +7,23 @@ import { faXmark } from "@fortawesome/free-solid-svg-icons";
 interface ModalProps {
     children: React.ReactNode
     closeModal: () => void
+    confirm?: {
+        text: string,
+        func: () => void
+    }
 }
 
-const Modal: React.FC<ModalProps> = ({ children, closeModal }) => {
+const Modal: React.FC<ModalProps> = ({ children, closeModal, confirm = null }) => {
     return (
         <div className={style.modalBackground}>
             <div className={style.modalContainer}>
                 {children}
-                <button onClick={closeModal}><FontAwesomeIcon icon={faXmark}/></button>
+                <button className={style.closeModal} onClick={closeModal}><FontAwesomeIcon icon={faXmark}/></button>
+                { confirm !== null ? 
+                    <button className={style.confirmButton} onClick={() => { confirm.func(); closeModal(); }}>
+                        {confirm.text}
+                    </button>
+                :  null }
             </div>
         </div>
     );
