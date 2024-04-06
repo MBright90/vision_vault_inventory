81ee442afaa0289def574de71c2e74fec4e091a7
mbright90 <michael.bright90@icloud.com>
2024-04-06 10:29:36 +0100

    Add editForm component directory

---
+++ client/src/components/editForm/EditForm.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import style from './EditForm.module.scss';
+
+const EditForm: React.FC = () => {
+    return (
+        <main className={style.editFormContainer}>
+            <p>This is the editing form</p>
+        </main>
+    );
+};
+
+export default EditForm;
