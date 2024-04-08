703018f8282d7f1ec4403f30462e436ca939920c
mbright90 <michael.bright90@icloud.com>
2024-04-08 17:12:18 +0100

    Add NavBar within routers scope

--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -1,12 +1,15 @@
-import Display from "@components/display/Display";
-import NewProductForm from "@components/newProductForm/NewProductForm";
+import Display from "@components/display";
+import NavBar from "@components/navbar";
+import NewProductForm from "@components/newProductForm";
 import React from "react";
-
 import { HashRouter, Route, Routes } from "react-router-dom";
+
+
 
 const RouteSwitch: React.FC = () => {
     return (
-        <HashRouter>
+        <HashRouter basename="/">
+            <NavBar />
             <Routes>
                 <Route path="/" element={<Display />} />
                 <Route path="/new" element={<NewProductForm />} />
