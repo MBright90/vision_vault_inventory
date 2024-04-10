468619fd466d9e1db7bdb6bf4a09b4d6e131024d
mbright90 <michael.bright90@icloud.com>
2024-04-10 11:08:10 +0100

    Add submit button

--- client/src/components/newProductForm/NewProductForm.tsx
+++ client/src/components/newProductForm/NewProductForm.tsx
@@ -3,29 +3,39 @@ import React from "react";
 import style from './NewProductForm.module.scss';
 
 const NewProductForm: React.FC = () => {
-    return (
+    return (<div className={style.formContainer}>
+        <h3>Add Product</h3>
         <form action="" className={style.newProductForm}>
 
-            <label htmlFor="name-input">
-                Product Name:
+            <div className={style.inputContainer}>
+                <label htmlFor="name-input">Product Name</label>
                 <input id="name-input" name="name-input" type="text" required />
-            </label>
+            </div>
 
-            <label htmlFor="description-input">
-                Description:
+            <div className={style.inputContainer}>
+                <label htmlFor="description-input">Description</label>
                 <textarea id="description-input" name="description-input" required />
-            </label>
+            </div>
 
-            <label htmlFor="stock-input">
-                Initial stock:
-                <input id="stock-input" name="stock-input" type="number" min={0} required />
-            </label>
+            <div className={style.inputContainer}>
+                <label htmlFor="stock-input">Initial stock</label>
+                <input id="stock-input" name="stock-input" type="number" max={100} min={0} required />
+                <p className={style.inputInfo}>
+                    Max 100
+                </p>
+            </div>
 
-            <label htmlFor="image-input">
-                Image:
-                <input id="image-input" name="image-input" type="text" required />
-            </label>
+            <div className={style.inputContainer}>
+                <label htmlFor="genres-input">Image</label>
+                <input id="genres-input" name="genres-input" type="text" required />
+                <p className={style.inputInfo}>
+                    Separate genres with a comma
+                </p>
+            </div>
+
+            <button className={style.formSubmitBtn} type="submit">Add Product</button>
         </form>
+    </div>
     );
 };
 
