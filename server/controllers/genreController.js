90b0f79e09c9a438e4c14d4ae1b967eb12c1895b
mbright90 <michael.bright90@icloud.com>
2024-05-13 16:55:26 +0100

    Handle options creation

--- server/controllers/genreController.js
+++ server/controllers/genreController.js
@@ -3,24 +3,25 @@
 const Genre = require('../models/genre');
 
 async function get_id(genre, session = null) {
+  console.log(genre);
   // find genre and create if not found
   try {
     if (genre === '') return '';
 
     const lowerGen = genre.toLowerCase();
-    const updateOptions = { name: lowerGen };
 
-    if (session) { updateOptions.session = session; }
+    const options = { upsert: true, new: true };
+    if (session) options.session = session;
 
     const result = await Genre.findOneAndUpdate(
       { name: lowerGen },
-      updateOptions,
-      { upsert: true, new: true, session },
+      { name: lowerGen },
+      options,
     );
     return result._id;
   } catch (err) {
     console.log(err); // log error
-    throw err;
+    return null;
   }
 }
 
@@ -50,10 +51,11 @@ async function add_product(genreId, productId, session) {
 
 async function remove_product(genreId, productId, session) {
   try {
+    const options = session ? { session } : {};
     const result = await Genre.updateOne(
       { _id: genreId },
       { $pull: { products: productId } },
-      { session },
+      options,
     );
     return result;
   } catch (err) {
