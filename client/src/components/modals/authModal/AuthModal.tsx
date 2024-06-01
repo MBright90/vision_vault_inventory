1b9c3b5b2bd04f55f849eb2e4d6542a8f5c80fae
mbright90 <michael.bright90@icloud.com>
2024-06-01 10:29:06 +0100

    Add conditional

--- client/src/components/modals/authModal/AuthModal.tsx
+++ client/src/components/modals/authModal/AuthModal.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 import style from './AuthModal.module.scss';
+import endpoint from "@utilities/endpoint";
 
 interface AuthModalProps {
     closeModal: () => void,
@@ -11,14 +12,20 @@ interface AuthModalProps {
         headers: {'Content-Type': string},
         body: string
     }
+    productId: string
     displayModalFunc: () => void
 }
 
-const AuthModal: React.FC<AuthModalProps> = ({ closeModal, reqOptions, displayModalFunc }) => {
-    const [currentPassword, setCurrentPassword] = useState<string>('')
+const AuthModal: React.FC<AuthModalProps> = ({ closeModal, reqOptions, productId, displayModalFunc }) => {
+    const [currentPassword, setCurrentPassword] = useState<string>('');
 
-    async function completeFunc() => {
-        console.log('confirm')
+    async function confirmFunc(): Promise<void> {
+        if (reqOptions.method === 'POST') {
+            const response = await fetch(`${endpoint}/edit/${productId}}`);
+        } else if (reqOptions.method === 'DELETE') {
+            const response = await fetch(`${endpoint}/delete/${productId}`);
+        }
+        console.log('confirm');
     }
 
     return (
@@ -28,12 +35,12 @@ const AuthModal: React.FC<AuthModalProps> = ({ closeModal, reqOptions, displayMo
                 <p>Enter admin password to confirm:</p>
                 <input 
                     className={style.passwordInput}
-                    onChange={setCurrentPassword('hello')}/>
+                    onChange={(e) => setCurrentPassword(e.target.value)}/>
                 <div className={style.buttonsContainer}>
-                    <button className={style.ModalButton} onClick={() => { void callback(); closeModal(); }}>
+                    <button className={style.ModalButton} onClick={() => { void confirmFunc(); }}>
                         Confirm
                     </button>
-                    <button className={style.ModalButton} onClick={() => { void callback(); closeModal(); }}>
+                    <button className={style.ModalButton} onClick={() => { closeModal(); }}>
                         Cancel
                     </button>
                 </div>
