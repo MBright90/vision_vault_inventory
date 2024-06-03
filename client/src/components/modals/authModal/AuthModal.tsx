4e8166fe832f158a8ac5db3ad6d380a880b1d230
mbright90 <michael.bright90@icloud.com>
2024-06-03 17:25:44 +0100

    Call displayModalFunc post fetch

--- client/src/components/modals/authModal/AuthModal.tsx
+++ client/src/components/modals/authModal/AuthModal.tsx
@@ -6,11 +6,22 @@ import style from './AuthModal.module.scss';
 import endpoint from "@utilities/endpoint";
 
 interface AuthModalProps {
-    closeModal: () => void,
+    closeModal: () => void
     reqOptions: { 
         method: "DELETE" | "POST",
         headers: {'Content-Type': string},
-        body: string
+        body: {
+            currentName: string | null
+            description: string | null
+            number_in_stock: number | null
+            genres: string | null
+            type: string | null
+            prevGenres: string[] | null
+            password: string | null
+
+            genreIds: string[] | null
+            typeId: string | null
+        }
     }
     productId: string
     displayModalFunc: () => void
@@ -20,12 +31,22 @@ const AuthModal: React.FC<AuthModalProps> = ({ closeModal, reqOptions, productId
     const [currentPassword, setCurrentPassword] = useState<string>('');
 
     async function confirmFunc(): Promise<void> {
+        reqOptions.body.password = currentPassword;
         if (reqOptions.method === 'POST') {
             const response = await fetch(`${endpoint}/edit/${productId}}`);
-        } else if (reqOptions.method === 'DELETE') {
+            if (response.ok) {
+                displayModalFunc();
+            } else {
+                displayModalFunc();
+            }
+        } else {
             const response = await fetch(`${endpoint}/delete/${productId}`);
+            if (response.ok) {
+                displayModalFunc();
+            } else {
+                displayModalFunc();
+            }
         }
-        console.log('confirm');
     }
 
     return (
@@ -35,7 +56,7 @@ const AuthModal: React.FC<AuthModalProps> = ({ closeModal, reqOptions, productId
                 <p>Enter admin password to confirm:</p>
                 <input 
                     className={style.passwordInput}
-                    onChange={(e) => setCurrentPassword(e.target.value)}/>
+                    onChange={(e) => { setCurrentPassword(e.target.value); }}/>
                 <div className={style.buttonsContainer}>
                     <button className={style.ModalButton} onClick={() => { void confirmFunc(); }}>
                         Confirm
