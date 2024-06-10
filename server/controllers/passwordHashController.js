1a2d34855d5ed1ab3026db9e3ad6c2b2824def56
mbright90 <michael.bright90@icloud.com>
2024-06-10 14:46:11 +0100

    Initiate passwordHashController

---
+++ server/controllers/passwordHashController.js
@@ -0,0 +1,15 @@
+/* eslint-disable camelcase */
+const PasswordHash = require('../models/passwordHash');
+
+async function get_password_hash() {
+  // get current hash of password
+}
+
+async function update_password_hash() {
+  // update currently stored hash
+}
+
+module.exports = {
+  get_password_hash,
+  update_password_hash,
+};
