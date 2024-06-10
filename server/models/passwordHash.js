3853dc91ad5d2c8f36ba7ef4e2c1641fe325eacc
mbright90 <michael.bright90@icloud.com>
2024-06-10 14:46:38 +0100

    File name change

---
+++ server/models/passwordHash.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const PasswordHashSchema = mongoose.Schema({
+  hash: { type: String, required: true },
+});
+
+module.exports = mongoose.model(PasswordHashSchema);
