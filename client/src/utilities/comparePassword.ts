117700e8134e86d00ab0eb5a2d848b2c47758260
mbright90 <michael.bright90@icloud.com>
2024-07-09 15:49:24 +0100

    Add response type interface

--- client/src/utilities/comparePassword.ts
+++ client/src/utilities/comparePassword.ts
@@ -1,11 +1,17 @@
 import bcrypt from "bcryptjs";
 import endpoint from "./endpoint";
 
+interface hashResponseObject {
+    _id: string
+    type: 'admin'
+    hash: string
+}
+
 async function comparePassword(password: string): Promise<boolean> {
     const response = await fetch(`${endpoint}/password/`);
-    const data = await response.json() as string;
+    const data = await response.json() as hashResponseObject;
 
-    return bcrypt.compareSync(password, data);
+    return bcrypt.compareSync(password, data.hash);
 };
 
 export default comparePassword;
