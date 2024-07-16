9b6d2b81ec0d5b18713d228393adf549e0c6a0ab
mbright90 <michael.bright90@icloud.com>
2024-07-16 17:20:12 +0100

    Update file extension & add snapshot test

--- client/src/components/footer/Footer.test.tsx
+++ client/src/components/footer/Footer.test.tsx
@@ -3,6 +3,12 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom"
 import Footer from "./Footer";
 
+test('matches snapshot', () => {
+    const { container } = render(<Footer />);
+
+    expect(container).toMatchSnapshot();
+})
+
 test('renders link to creators github page', () => {
     render(<Footer />);
     const aLink = screen.getByText(/MBright90/);
