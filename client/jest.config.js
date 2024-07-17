46ffeb9851e5c6f1e7bac3651b8f99798fc51c21
mbright90 <michael.bright90@icloud.com>
2024-07-17 16:17:34 +0100

    Add aliases for JEST config

--- client/jest.config.js
+++ client/jest.config.js
@@ -6,6 +6,14 @@ module.exports = {
     '^.+\\.(js|jsx)$': 'babel-jest',
   },
   moduleNameMapper: {
+    // aliases
+    '^@assets': '<rootDir>/src/assets',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@custom_types/(.*)$': '<rootDir>/src/types/$1',
+    '^@utilities/(.*)$': '<rootDir>/src/utilities/$1',
+
+
+    // mocks
     '\\.(s)?css$': '<rootDir>/test/jest/__mocks__/styleMock.js',
   }
 };
