0347af2331aa08237f030f3b595f196983be64fe
mbright90 <michael.bright90@icloud.com>
2024-07-21 17:01:22 +0100

    Add form submission tests

--- client/src/components/newProductForm/NewProductForm.test.tsx
+++ client/src/components/newProductForm/NewProductForm.test.tsx
@@ -19,16 +19,7 @@ jest.mock('@components/modals/confirmModal/ConfirmModal', () => ({
 jest.mock('@utilities/retrieveTypes', () => jest.fn());
 
 beforeAll(async () => {
-    global.fetch = jest.fn((): Promise<Response> =>
-        Promise.resolve({
-            ok: true,
-            json: async (): Promise<{err: string}> => Promise.resolve(
-                {
-                    err: 'Error message'
-                }
-            )
-        } as Response)
-    );
+    global.fetch = jest.fn();
 });
 
 beforeEach(() => {
@@ -39,16 +30,22 @@ test('matches snapshot', () => {
     const { container } = render(<NewProductForm />);
     
     expect(container).toMatchSnapshot();
-})
+});
+
+test('calls retrieveTypes on component mount', () => {
+    render(<NewProductForm />);
+
+    expect(retrieveTypes).toHaveBeenCalledTimes(1);
+});
 
 test('renders form with initial empty values', () => {
     render(<NewProductForm />);
 
     expect(screen.getByLabelText(/Product Name/i)).toHaveValue('');
     expect(screen.getByLabelText(/Description/i)).toHaveValue('');
-    expect(screen.getByLabelText(/Price/i)).toHaveValue('0');
-    expect(screen.getByLabelText(/Initial Stock/i)).toHaveValue('0');
-    expect(screen.getByLabelText(/Type/i)).toHaveValue('');
+    expect(screen.getByLabelText(/Price/i)).toHaveValue(0);
+    expect(screen.getByLabelText(/Initial Stock/i)).toHaveValue(0);
+    expect(screen.getByLabelText(/Type/i)).toHaveValue('Select Type...');
     expect(screen.getByLabelText(/Genres/i)).toHaveValue('');
 });
 
@@ -65,7 +62,7 @@ test('updates the product description input value when typed into', () => {
 
     const descriptionInput = screen.getByLabelText(/Description/i);
     fireEvent.change(descriptionInput, { target: { value: 'Test Product Description' } });
-    expect(descriptionInput).toHaveValue('Test product Description');
+    expect(descriptionInput).toHaveValue('Test Product Description');
 });
 
 test('updates the product price input value when typed into', () => {
@@ -73,7 +70,7 @@ test('updates the product price input value when typed into', () => {
 
     const priceInput = screen.getByLabelText(/Price/i);
     fireEvent.change(priceInput, { target: { value: '100' } });
-    expect(priceInput).toHaveValue('100');
+    expect(priceInput).toHaveValue(100);
 });
 
 test('updates the product initial stock input value when typed into', () => {
@@ -81,7 +78,7 @@ test('updates the product initial stock input value when typed into', () => {
 
     const stockInput = screen.getByLabelText(/Price/i);
     fireEvent.change(stockInput, { target: { value: '200' } });
-    expect(stockInput).toHaveValue('200');
+    expect(stockInput).toHaveValue(200);
 });
 
 test('updates the product genres input value when typed into', () => {
@@ -92,10 +89,58 @@ test('updates the product genres input value when typed into', () => {
     expect(genresInput).toHaveValue('test genre one, test genre two');
 });
 
-test('calls retrieveTypes on component mount', () => {
+test('form is submitted when submit button is clicked', async () => {
     render(<NewProductForm />);
 
-    expect(retrieveTypes).toHaveBeenCalledTimes(1);
+    const submitButton = screen.getByRole('button');
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+        expect(fetch).toHaveBeenCalled();
+    });
+});
+
+test('upon successful form submission, a modal shows success message', async () => {
+    global.fetch = jest.fn((): Promise<Response> => 
+        Promise.resolve({
+            ok: true,
+            json: async () => {},
+        } as Response)
+    );
+    
+    render(<NewProductForm />);
+
+    const submitButton = screen.getByRole('button');
+    fireEvent.click(submitButton);
+    expect(fetch).toHaveBeenCalled();
+
+    await waitFor(() => {
+        expect(screen.getByTestId('mock-confirm-modal')).toBeInTheDocument();
+        expect(screen.getByText('Product added successfully')).toBeInTheDocument();
+    });
+});
+
+test('upon unsuccessful form submission, a modal shows an error message', async () => {
+    global.fetch = jest.fn((): Promise<Response> => 
+        Promise.resolve({
+            ok: false,
+            json: async (): Promise<{ err: string }> => Promise.resolve({
+                err: 'test error message'
+            }),
+        } as Response)
+    );
+    
+    render(<NewProductForm />);
+
+    const submitButton = screen.getByRole('button');
+    fireEvent.click(submitButton);
+    expect(fetch).toHaveBeenCalled();
+
+    await waitFor(() => {
+        expect(screen.getByTestId('mock-confirm-modal')).toBeInTheDocument();
+        expect(screen.getByText(/Error adding product/i)).toBeInTheDocument();
+        expect(screen.getByText(/test error message/i)).toBeInTheDocument();
+    });
 });
 
 test('handles component unmounting without errors', () => {
