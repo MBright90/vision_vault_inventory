d24d0f4ad3b81e602e5908b14df3e8cfe3d232d7
mbright90 <michael.bright90@icloud.com>
2024-07-21 17:00:45 +0100

    Linting fix

--- client/src/components/display/Display.test.tsx
+++ client/src/components/display/Display.test.tsx
@@ -14,7 +14,7 @@ jest.mock('@components/productDisplay/ProductDisplay', () => ({
         </div>
       );
     }
-  }));
+}));
 
 beforeAll(async () => {
     global.fetch = jest.fn((): Promise<Response> =>
