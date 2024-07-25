85a3a04d3d58113a25a671bd344dd8f3481406dc
mbright90 <michael.bright90@icloud.com>
2024-07-25 10:53:24 +0100

    Add further ProductDisplay tests

--- client/src/components/productDisplay/ProductDisplay.test.tsx
+++ client/src/components/productDisplay/ProductDisplay.test.tsx
@@ -30,25 +30,62 @@ beforeEach(() => {
     global.fetch = jest.fn(() =>
         Promise.resolve({
             ok: true,
-            json: async () => ({
-                name: 'test-name',
-                description: 'test-description',
-                price: 1,
-                number_in_stock: 2,
-                type: { name: 'test-type-name' },
-                genres: [
-                    { name: 'test-genre-1', _id: 'test-genre-id-1' },
-                    { name: 'test-genre-2', _id: 'test-genre-id-2' },
-                ],
-            }),
+            json: async () => ([
+                {
+                    _id: 'test-id-1',
+                    name: 'test-name',
+                    description: 'test-description',
+                    price: 1,
+                    number_in_stock: 2,
+                    type: { name: 'test-type-name' },
+                    genres: [
+                        { name: 'test-genre-1', _id: 'test-genre-id-1' },
+                        { name: 'test-genre-2', _id: 'test-genre-id-2' },
+                    ],
+                }, {
+                    _id: 'test-id-2',
+                    name: 'test-name-2',
+                    description: 'test-description-2',
+                    price: 10,
+                    number_in_stock: 20,
+                    type: { name: 'test-type-name' },
+                    genres: [
+                        { name: 'test-genre-1', _id: 'test-genre-id-1' },
+                        { name: 'test-genre-2', _id: 'test-genre-id-2' },
+                    ],  
+                }
+            ]),
         } as Response)
     );
 });
 
 test('matches snapshot', async () => {
-    const { container } = render(<ProductDisplay genreId={'all'}/>);
+    const { container } = render(<ProductDisplay genreId="all"/>);
 
     await waitFor(() => {
         expect(container).toMatchSnapshot();
+    });
+});
+
+test('displays no products message when no products are retrieved', async () => {
+    global.fetch = jest.fn(() =>
+    Promise.resolve({
+        ok: true,
+        json: async () => ([]),
+    } as Response)
+);
+    render(<ProductDisplay genreId="all"/>);
+
+    await waitFor(() => {
+        expect(screen.getByText(/No products matching search parameters/i)).toBeInTheDocument();
+    });
+});
+
+test('displays a retrieved products name, description and type', async () => {
+    render(<ProductDisplay genreId="all" />);
+
+    await waitFor(() => {
+        expect(screen.getByText(/^test-name$/i)).toBeInTheDocument();
+        expect(screen.getByText(/^test-description$/i)).toBeInTheDocument();
     });
 });
