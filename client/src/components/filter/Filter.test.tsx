78bc8694daf7463c94c15121c9cdf2ccb98940f3
mbright90 <michael.bright90@icloud.com>
2024-08-01 10:26:57 +0100

    Add go back tests

--- client/src/components/filter/Filter.test.tsx
+++ client/src/components/filter/Filter.test.tsx
@@ -8,10 +8,41 @@ jest.mock('@utilities/retrieveTypes');
 const mockGoBack = jest.fn();
 const mockUpdateFilter = jest.fn();
 
+beforeEach(() => {
+    jest.resetAllMocks();
+});
+
 test('matches snapshot', () => {
     const { container } = render(
         <Filter goBack={mockGoBack} productIsActive={false} updateFilter={mockUpdateFilter}/>
     );
 
     expect(container).toMatchSnapshot();
+});
+
+test('go back button is visible when product is active', () => {
+    render(
+        <Filter goBack={mockGoBack} productIsActive={true} updateFilter={mockUpdateFilter}/>
+    );
+
+    expect(screen.getByText(/Go Back/i)).toBeInTheDocument();
+});
+
+test('go back button not visible when no product is active', () => {
+    render(
+        <Filter goBack={mockGoBack} productIsActive={false} updateFilter={mockUpdateFilter}/>
+    );
+
+    expect(screen.queryByText(/Go Back/i)).not.toBeInTheDocument();
+});
+
+test('calls goBack when go back button fires click event', () => {
+    render(
+        <Filter goBack={mockGoBack} productIsActive={true} updateFilter={mockUpdateFilter}/>
+    );
+
+    const goBackBtn = screen.getByText(/Go Back/i);
+    fireEvent.click(goBackBtn);
+
+    expect(mockGoBack).toHaveBeenCalled();
 });
