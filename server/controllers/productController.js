3ccb280c545a0c497e136f3af8dfde35cc8cffdc
mbright90 <michael.bright90@icloud.com>
2024-08-20 17:07:16 +0100

    Refactor validateProduct

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -4,19 +4,7 @@ const { default: mongoose } = require('mongoose');
 const Product = require('../models/product');
 const genreController = require('./genreController');
 const typeController = require('./typeController');
-
-function validateProduct(product) {
-  if (product.name.length < 2 || product.name.length > 150) return { err: 'Name does not meet requirements' };
-  if (product.description.length > 500) return { err: 'Description exceeds permitted length of 500 characters' };
-  if (product.price <= 0) return { err: 'Price incorrectly set' };
-  if (product.number_in_stock <= 0) return { err: 'Stock incorrectly set' };
-  if (product.genres.length <= 0) return { err: 'Minimum genres not met' };
-  if (
-    product.type.length <= 0
-    || !['hardware', 'video/disc', 'game/disc', 'book'].includes(product.type.toLowerCase())
-  ) return { err: 'Invalid product type chosen' };
-  return true;
-}
+const validateProduct = require('../validators/productValidator');
 
 async function get_all(req, res) {
   try {
